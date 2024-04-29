9436e3c950d38e53da79aeccae9201fe07d0b8f6
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-04-29 19:36:31 -0300

    adding SBLoanView on Classes

---
+++ SBLoan/Classes/SBLoanView.swift
@@ -0,0 +1,21 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by Adriano Valumin on 29/04/24.
+//
+
+import SwiftUI
+
+public struct SBLoanView: View {
+
+    public init() { }
+
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
