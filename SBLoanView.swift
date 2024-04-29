934c0f0990eb95c1ab1e0dd725a8f5d16ef966b6
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-04-29 19:21:31 -0300

    Initial commit

---
+++ SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by Adriano Valumin on 29/04/24.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
